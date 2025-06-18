a7ceec35a8f5c15e50bf9e07751509c18199be5f
ngockhanhdo0211 <ngockhanhdo0211@gmail.com>
2025-06-18 08:09:39 +0700

    video 20

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 console.log("Hello world")
 console.log("hoidanit")
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+app.get("/", (req, res) => {
+  res.send ("Hello world")
+})
+app.get("/hoidanit", (req, res) => {
+  res.send ("Hello nkhanhday")
+})
+app.listen(PORT, ()=> {
+  console.log('My app is running on port: 8080')
+})
